5a23820a21ce4f6e631834aeee5fc00cf23e87fa
Michael Brandt <mib@unik.dk>
2020-01-15 14:50:20 +0100

    WIP eslint

--- code/packages/eslint/src/rules/yield-return-type.js
+++ code/packages/eslint/src/rules/yield-return-type.js
@@ -13,14 +13,25 @@ module.exports = {
   },
   create: context => {
     let methods = ["call"];
-    if (context.options.length >= 1){
+    if (context.options.length >= 1) {
       methods = context.options[0];
     }
     console.log("eslint-plugin-yield-return-type methods: ", methods);
     return {
-      "FunctionDeclaration": node => {
-        console.log(node);
+      VariableDeclarator: node => {
+        if (node.init && node.init.type === "YieldExpression") {
+          if (
+            node.init.argument.type === "CallExpression" &&
+            node.init.argument.arguments.lengt > 0 &&
+            methods.some(x => x === node.init.argument.arguments[0])
+          ) {
+            if (node.id.type === "Identifier") {
+              if (node.id.typeAnnotation === undefined)
+                context.report({ node, message: "TypeAnnotation is missing." });
+            }
+          }
+        }
       }
     };
   }
-}
+};
